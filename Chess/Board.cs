2fbd1660237f7123a97035f77bb97b6759cf9815
Daniel Waugh <dwaugh@sdsu.edu>
2018-12-03 17:01:42 -0800

    Updated Comments for my portion of code

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -10,26 +10,28 @@ namespace Chess
 {
     class Board
     {
-        private int[,] piecePositions = new int[8, 8]       //holds the position of all the pieces on the 8x8 board
+        /// <summary>
+        /// holds the position of all the pieces on the 8x8 board (total of 32 pieces represented by corresponding
+        /// numbers, 0 represents empty position). These numbers map to the name of the xaml object in the uwp application
+        /// </summary>
+        private int[,] piecePositions = new int[8, 8]      
             { {1, 2, 3, 4, 5, 6, 7, 8}, {9, 10, 11, 12, 13, 14, 15, 16}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {17, 18, 19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30, 31, 32} };
 
         public int[,] selectedPiece = new int[8, 8];         //holds the position of the currently selected piece
 
-        private int[,] emptyLocations = new int[8, 8];         //holds the position of the currently selected piece
+        private List<Button> createdButtons = new List<Button>();   //holds a list of the buttons created when a piece is selected (desitination buttons)
 
-        private List<Button> emptyButtons = new List<Button>();
+        private Button currentPiece = new Button(); //holds the current piece that is selected
 
-        private Button currentPiece = new Button();
-
-        public bool finishedMoveFlag = false;
+        public bool finishedMoveFlag = false;   //set when a move is finished
 
         private int turn;       //holds whos turn it is
 
 
         public Board()
         {
-            turn = 0;
+            turn = 0;   //turn starts out with white pieces
         }
 
         public void SelectPiece(int pieceNumber, Button piece)
@@ -42,7 +44,7 @@ namespace Chess
                 {
                     if (piecePositions[i, j] == pieceNumber)
                     {
-                        selectedPiece[i, j] = 1;
+                        selectedPiece[i, j] = 1;    //puts the location of the selected piece in the grid array
                     }
                 }
             }
@@ -56,16 +58,15 @@ namespace Chess
                     {
                         if (thisRook.validMoveLocations[i,j] == true)
                         {
-                            Button curButton = new Button();
+                            Button curButton = new Button();    //creates button
                             curButton.Width = 72;
                             curButton.Height = 72;
-                            curButton.Name = $"{i + 1},{j + 1}";
-                            Grid.SetRow(curButton, i + 1);
+                            curButton.Name = $"{i + 1},{j + 1}";    //gives it a name for indexing later (name: "grid row, grid column")
+                            Grid.SetRow(curButton, i + 1);  //sets the location in the grid
                             Grid.SetColumn(curButton, j + 1);
-                            curButton.Click += Move;
-                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);
-                            emptyLocations[i, j] = 1;
-                            emptyButtons.Add(curButton);
+                            curButton.Click += Move;    //sets method to run when button is pressed
+                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);    //adds button to grid 
+                            createdButtons.Add(curButton);
                         }
                     }
                 }
@@ -81,16 +82,15 @@ namespace Chess
                     {
                         if (thisBishop.validMoveLocations[i, j] == true)
                         {
-                            Button curButton = new Button();
+                            Button curButton = new Button();    //creates button
                             curButton.Width = 72;
                             curButton.Height = 72;
-                            curButton.Name = $"{i + 1},{j + 1}";
-                            Grid.SetRow(curButton, i + 1);
+                            curButton.Name = $"{i + 1},{j + 1}";    //gives it a name for indexing later (name: "grid row, grid column")
+                            Grid.SetRow(curButton, i + 1);  //sets the location in the grid
                             Grid.SetColumn(curButton, j + 1);
-                            curButton.Click += Move;
-                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);
-                            emptyLocations[i, j] = 1;
-                            emptyButtons.Add(curButton);
+                            curButton.Click += Move;    //sets method to run when button is pressed
+                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);     //adds button to grid 
+                            createdButtons.Add(curButton);
                         }
                     }
                 }
@@ -106,16 +106,15 @@ namespace Chess
                     {
                         if (thisQueen.validMoveLocations[i, j] == true)
                         {
-                            Button curButton = new Button();
+                            Button curButton = new Button();    //creates button
                             curButton.Width = 72;
                             curButton.Height = 72;
-                            curButton.Name = $"{i + 1},{j + 1}";
-                            Grid.SetRow(curButton, i + 1);
+                            curButton.Name = $"{i + 1},{j + 1}";    //gives it a name for indexing later (name: "grid row, grid column")
+                            Grid.SetRow(curButton, i + 1);  //sets the location in the grid
                             Grid.SetColumn(curButton, j + 1);
-                            curButton.Click += Move;
-                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);
-                            emptyLocations[i, j] = 1;
-                            emptyButtons.Add(curButton);
+                            curButton.Click += Move;    //sets method to run when button is pressed
+                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);     //adds button to grid 
+                            createdButtons.Add(curButton);
                         }
                     }
                 }
@@ -128,16 +127,15 @@ namespace Chess
                     {
                         if (piecePositions[i, j] == 0)
                         {
-                            Button curButton = new Button();
+                            Button curButton = new Button();    //creates button
                             curButton.Width = 72;
                             curButton.Height = 72;
-                            curButton.Name = $"{i + 1},{j + 1}";
-                            Grid.SetRow(curButton, i + 1);
+                            curButton.Name = $"{i + 1},{j + 1}";    //gives it a name for indexing later (name: "grid row, grid column")
+                            Grid.SetRow(curButton, i + 1);  //sets the location in the grid
                             Grid.SetColumn(curButton, j + 1);
-                            curButton.Click += Move;
-                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);
-                            emptyLocations[i, j] = 1;
-                            emptyButtons.Add(curButton);
+                            curButton.Click += Move;    //sets method to run when button is pressed
+                            UIGlobal.XAMLpage.getGrid().Children.Add(curButton);     //adds button to grid 
+                            createdButtons.Add(curButton);
                         }
                         if (piecePositions[i, j] == pieceNumber)
                         {
@@ -149,76 +147,86 @@ namespace Chess
             
         }
 
-        public void UnselectPiece(int pieceNumber)
+        /// <summary>
+        /// unselects the piece by deleting all buttons created by the select piece function
+        /// </summary>
+        /// <param name="pieceNumber"></param>
+        public void UnselectPiece(int pieceNumber)  
         {
-            foreach(Button clearbutton in emptyButtons)
+            foreach(Button clearbutton in createdButtons)     //goes through emptybutton locations (places where the piece can go)
             {
-                UIGlobal.XAMLpage.getGrid().Children.Remove(clearbutton);
+                UIGlobal.XAMLpage.getGrid().Children.Remove(clearbutton);       //removes the buttons from the board
             }
-            Array.Clear(emptyLocations, 0, emptyLocations.Length);
-            Array.Clear(selectedPiece, 0, selectedPiece.Length);
-            emptyButtons.Clear();
+            Array.Clear(selectedPiece, 0, selectedPiece.Length);        //clears the aray of location of selected piece
+            createdButtons.Clear();     //clears the createdButtons list (so that it can be used again)
         }
 
+        /// <summary>
+        /// Moves the piece button from the current location to the place wehre the 
+        /// user selected it to move. This occurs when any of the created Buttons are pressed
+        /// </summary>
         private void Move(object sender, RoutedEventArgs e)
         {
-            int curPieceNumber = Convert.ToInt32(currentPiece.Name.Substring(1));
+            int curPieceNumber = Convert.ToInt32(currentPiece.Name.Substring(1));   //gets the current pieces identification number for xaml
 
-            Button curButton = (Button)sender;
-            string[] rowCol = curButton.Name.Split(',');
+            Button curButton = (Button)sender;      //casts the object to button
+            string[] rowCol = curButton.Name.Split(',');    //splits the name of the created button to get the row and column of it's location
 
-            Grid.SetRow(currentPiece, Convert.ToInt32(rowCol[0]));
-            Grid.SetColumn(currentPiece, Convert.ToInt32(rowCol[1]));
+            Grid.SetRow(currentPiece, Convert.ToInt32(rowCol[0]));  //sets the row of the current piece selected to row of button pressed
+            Grid.SetColumn(currentPiece, Convert.ToInt32(rowCol[1]));   //sets the col of the current piece selected to col of button pressed
 
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    if (piecePositions[i, j] == curPieceNumber)
+                    if (piecePositions[i, j] == curPieceNumber)     //clears the piece positions in the back end grid of where the piece used to be
                     {
                         piecePositions[i, j] = 0;
                     }
                 }
             }
-            piecePositions[Convert.ToInt32(rowCol[0]) - 1, Convert.ToInt32(rowCol[1]) - 1] = curPieceNumber;
-            UnselectPiece(curPieceNumber);
-            finishedMoveFlag = true;
-            ChangeTurn();
+            piecePositions[Convert.ToInt32(rowCol[0]) - 1, Convert.ToInt32(rowCol[1]) - 1] = curPieceNumber;    //stores the new location of the selected piece
+            UnselectPiece(curPieceNumber);  //unselects the piece by using the unselect function
+            finishedMoveFlag = true;    //move is now finished
+            ChangeTurn();       //changes the turn to the other player
         }
 
+        /// <summary>
+        /// handles changing the turn so only one users pieces are usable at a time
+        /// </summary>
         public void ChangeTurn()
         {
-            if (turn == 0)
+            if (turn == 0)  //turn = 0 is white, turn = 1 is black
             {
-                for(int i = 1; i < 17; i++)
+                for(int i = 1; i < 17; i++)     //sets all the white buttons to an opacity of .7 and disables the buttons
                 {
-                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;
+                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;   //gets piece by name
                     curButton.IsEnabled = false;
                     curButton.Opacity = .7;
                 }
-                for(int i = 17; i < 33; i++)
+                for(int i = 17; i < 33; i++)    //sets all the black buttons to an opacity of 1 and enables the buttons
                 {
-                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;
+                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;   //gets piece by name
                     curButton.IsEnabled = true;
                     curButton.Opacity = 1;
                 }
-                turn = 1;
+                turn = 1;   //changes turn
             }
             else
             {
-                for (int i = 1; i < 17; i++)
+                for (int i = 1; i < 17; i++)    //sets all the white buttons to an opacity of 1 and enables the buttons
                 {
-                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;
+                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;   //gets piece by name
                     curButton.IsEnabled = true;
                     curButton.Opacity = 1;
                 }
-                for (int i = 17; i < 33; i++)
+                for (int i = 17; i < 33; i++)   //sets all the black buttons to an opacity of .7 and disables the buttons
                 {
-                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;
+                    Button curButton = UIGlobal.XAMLpage.FindName($"p{i}") as Button;   //gets piece by name
                     curButton.IsEnabled = false;
                     curButton.Opacity = .7;
                 }
-                turn = 0;
+                turn = 0;   //changees turn
             }
         }
     }
--- Chess/MainPage.xaml.cs
+++ Chess/MainPage.xaml.cs
@@ -18,55 +18,53 @@ namespace Chess
 {
     public sealed partial class MainPage : Page
     {
-        Board chessBoard = new Board();
+        Board chessBoard = new Board();     //create new board object
 
-        bool selectFlag = true;
+        bool selectFlag = true;     //for selecting pieces. if it is true, it means no piece is selected. if it is false, the piece is not selected
 
         public MainPage()
         {
-            this.InitializeComponent();
-            UIGlobal.XAMLpage = this;
+            this.InitializeComponent();     //initializes all the xaml objects and pre work for app
+            UIGlobal.XAMLpage = this;       //for use of making xaml objects public for other classes
         }
 
         public Grid getGrid()
         {
-            return maingrid;
+            return maingrid;        //returns the grid, so that the chess pieces can be used in external classes
         }
-        private void SelectPiece(object sender, RoutedEventArgs e)
+        private void SelectPiece(object sender, RoutedEventArgs e)      //button action for all the pieces
         {
-            Button currentPieceButton = (Button)sender;
-            int currentPieceNumber = 0;
-            currentPieceNumber = Convert.ToInt32(currentPieceButton.Name.Substring(1));
+            Button currentPieceButton = (Button)sender; //cast the object to a button type
+            int currentPieceNumber = 0;     //to store current pieces number
+            currentPieceNumber = Convert.ToInt32(currentPieceButton.Name.Substring(1)); //sets the current piece number to the xaml name (representing the numbered piece)
 
-            if (selectFlag || chessBoard.finishedMoveFlag)
+            if (selectFlag || chessBoard.finishedMoveFlag)      //if a piece is not selected or a move just finished
             {
-                chessBoard.SelectPiece(currentPieceNumber, currentPieceButton);
-                selectFlag = false;
+                chessBoard.SelectPiece(currentPieceNumber, currentPieceButton);     //run the board functionn that selects the piece and creates locations it can go
+                selectFlag = false;     //tells the program that a piece has been selected
             }
             else
             {
-                chessBoard.UnselectPiece(currentPieceNumber);
-                selectFlag = true;
+                chessBoard.UnselectPiece(currentPieceNumber);       //unselects piece only if it is already selected
+                selectFlag = true;      //tells the program that the piece has been unselected
             }
 
 
         }
 
-        /*private void PawnSingleMoveWhite(object sender, RoutedEventArgs e)
-        {
-            Button curButton = sender as Button;
-            int currentRow = Grid.GetRow(curButton);
-            Grid.SetRow(curButton, ++currentRow);
-        }*/
-
     }
-    public static class UIGlobal
+    /// <summary>
+    /// This class handles making the grid public to other classes. Without this,
+    /// it would not be possible to modify the xaml file outside of the xaml main
+    /// class. It is static (unchanging) and returns the grid of the xaml class. 
+    /// </summary>
+    public static class UIGlobal        
     {
-        public static MainPage XAMLpage { get; set; }
+        public static MainPage XAMLpage { get; set; }       //create the main page object
 
         public static Grid getGrid()
         {
-            return XAMLpage.getGrid();
+            return XAMLpage.getGrid();  //returns the grid object to be used in external classes
         }
     }
 }
