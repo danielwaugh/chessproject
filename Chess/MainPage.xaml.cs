3d03b61c476df190bd3eccb0aeb424b1ba1a6ace
Daniel Waugh <dwaugh@sdsu.edu>
2018-12-08 09:03:55 -0800

    Menus complete. All that needs to be added is comments and functionality for save game and load game.

--- Chess/MainPage.xaml.cs
+++ Chess/MainPage.xaml.cs
@@ -18,16 +18,20 @@ namespace Chess
 {
     public sealed partial class MainPage : Page
     {
-        Board chessBoard = new Board();     //create new board object
+        Board chessBoard;     //create new board object
 
         bool selectFlag = true;     //for selecting pieces. if it is true, it means no piece is selected. if it is false, the piece is not selected
 
         Grid startMenu = new Grid();
 
+        Grid popUpMenu = new Grid();
+
         public MainPage()
         {
             this.InitializeComponent();     //initializes all the xaml objects and pre work for app
             UIGlobal.XAMLpage = this;       //for use of making xaml objects public for other classes
+            popUpMenu = (Grid)this.FindName("gamepopupmenu");
+            this.getGrid().Children.Remove(popUpMenu);
         }
 
         public Grid getGrid()
@@ -60,11 +64,152 @@ namespace Chess
             player1text.Opacity = 1;
             TextBlock player2text = (TextBlock)this.FindName("player2captured");
             player2text.Opacity = 1;
+            chessBoard = new Board();
+            RearangePieces();
         }
 
         private void LoadGame(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void MainMenu(object sender, RoutedEventArgs e)
+        {
+            getGrid().Children.Remove(popUpMenu);
+            getGrid().Children.Add(startMenu);
+            startMenu.Opacity = 1;
+        }
+
+        private void SaveGame(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReturnToGame(object sender, RoutedEventArgs e)
+        {
+            TextBlock player1text = (TextBlock)this.FindName("player1captured");
+            player1text.Opacity = 1;
+            TextBlock player2text = (TextBlock)this.FindName("player2captured");
+            player2text.Opacity = 1;
+            getGrid().Children.Remove(popUpMenu);
+        }
+
+        private void GameMenu(object sender, RoutedEventArgs e)
+        {
+            TextBlock player1text = (TextBlock)this.FindName("player1captured");
+            player1text.Opacity = 0;
+            TextBlock player2text = (TextBlock)this.FindName("player2captured");
+            player2text.Opacity = 0;
+            getGrid().Children.Add(popUpMenu);
+            popUpMenu.Opacity = 1;
+        }
+
+        private void RearangePieces()
+        {
+            for(int i = 1; i < 9; i++)
+            {
+                Button curPiece = (Button)this.FindName($"p{i}");
+                Grid parent = (Grid)curPiece.Parent;
+                if(parent.Name == "maingrid")
+                {
+                    Grid.SetRow(curPiece, 1);
+                    Grid.SetColumn(curPiece, i);
+                }
+                else if (parent.Name == "player1grid")
+                {
+                    UIGlobal.getPlayer1Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 1);
+                    Grid.SetColumn(curPiece, i);
+                }
+                else if (parent.Name == "player2grid")
+                {
+                    UIGlobal.getPlayer2Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 1);
+                    Grid.SetColumn(curPiece, i);
+                }
+                curPiece.IsEnabled = true;
+                curPiece.Opacity = 1;
+            }
+            for (int i = 9, j = 1; j < 9 && i < 17; i++, j++)
+            {
+                Button curPiece = (Button)this.FindName($"p{i}");
+                Grid parent = (Grid)curPiece.Parent;
+                if (parent.Name == "maingrid")
+                {
+                    Grid.SetRow(curPiece, 2);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player1grid")
+                {
+                    UIGlobal.getPlayer1Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 2);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player2grid")
+                {
+                    UIGlobal.getPlayer2Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 2);
+                    Grid.SetColumn(curPiece, j);
+                }
+                curPiece.IsEnabled = true;
+                curPiece.Opacity = 1;
+            }
+            for (int i = 17, j = 1; j < 9 && i < 25; i++, j++)
+            {
+                Button curPiece = (Button)this.FindName($"p{i}");
+                Grid parent = (Grid)curPiece.Parent;
+                if (parent.Name == "maingrid")
+                {
+                    Grid.SetRow(curPiece, 7);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player1grid")
+                {
+                    UIGlobal.getPlayer1Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 7);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player2grid")
+                {
+                    UIGlobal.getPlayer2Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 7);
+                    Grid.SetColumn(curPiece, j);
+                }
+                curPiece.IsEnabled = false;
+                curPiece.Opacity = 0.7;
+            }
+            for (int i = 25, j = 1; j < 9 && i < 33; i++, j++)
+            {
+                Button curPiece = (Button)this.FindName($"p{i}");
+                Grid parent = (Grid)curPiece.Parent;
+                if (parent.Name == "maingrid")
+                {
+                    Grid.SetRow(curPiece, 8);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player1grid")
+                {
+                    UIGlobal.getPlayer1Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 8);
+                    Grid.SetColumn(curPiece, j);
+                }
+                else if (parent.Name == "player2grid")
+                {
+                    UIGlobal.getPlayer2Grid().Children.Remove(curPiece);
+                    getGrid().Children.Add(curPiece);
+                    Grid.SetRow(curPiece, 8);
+                    Grid.SetColumn(curPiece, j);
+                }
+                curPiece.IsEnabled = false;
+                curPiece.Opacity = 0.7;
+            }
         }
     }
 
